55d5e2c140382c94788f8618aaef4f7ac5f8d70c
ishak <ileghlam@student.42.fr>
2017-09-30 10:59:43 +0200

    remove comment

--- test/utils/createKeystore.js
+++ test/utils/createKeystore.js
@@ -15,33 +15,9 @@ export const createVault = data =>
 
 export const entropy = 123;
 export const password = 'Dether@1';
-// 
-// const loadKeystore = (keystore, password) => dispatch => (
-//   new Promise((res, rej) => {
-//     keystore.keyFromPassword(password, (error, pwDerivedKey) => {
-//       if (error) return rej(error);
-//       keystore.generateNewAddress(pwDerivedKey, 1);
-//       const addr = keystore.getAddresses()[0];
-//       dispatch(setKeystore(keystore));
-//       dispatch(setAllTsx([]));
-//       return dispatch(checkNetwork()).then(() => {
-//         dispatch(getBasicEthInfo(addr));
-//         res(keystore);
-//       });
-//     });
-//   })
-// );
 
 export const createKeystore = async () => {
   const extraEntropy = entropy.toString();
   const seed = await generateRandomSeed(extraEntropy);
   return createVault({ password, seedPhrase: seed, hdPathString: "m/44'/60'/0'/0" });
-  // const derivKey = await ks.keyFromPassword(password)
-  //
-  // // , (err, derivKey) => {
-  //   ks.generateNewAddress(derivKey, 1);
-  //   const addr = ks.getAddresses()[0];
-  //   console.log(addr);
-  //   return addr;
-  // });
 };
