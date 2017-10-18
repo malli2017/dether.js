272da51be16326010c4c602fb75d461fe96106a8
Fukunaga42 <mehdi.amari@gmail.com>
2017-10-18 18:31:49 +0200

    add mock contract

---
+++ test/mock/contract-mock.js
@@ -0,0 +1,57 @@
+import BigNumber from "bignumber.js";
+
+const instance = {
+  getTellerPos: () =>
+      new Promise(resolve => {
+        resolve([1230000, 1110000, 42, 10000]);
+      }),
+  getTellerProfile: () =>
+      new Promise(resolve => {
+        resolve([2300, 10000, 200000, 'name', 3, 3, 'telegram']);
+      }),
+  sendCoin: () =>
+      new Promise(resolve => {
+        resolve({
+          logs: [{
+          event: 'SendCoin',
+          args: {
+            _from: '0x000000000000', _to: '0x100000000000', amount: 10000000, lat: 123000, lng: 321000,
+          },
+        }],
+      });
+    }),
+  withdrawAll: () =>
+      new Promise(resolve => {
+        resolve({
+          logs: [{
+            event: 'Withdraw',
+            args: {
+              amount: 12300000, lat: 123000, lng: 321000,
+            },
+          }],
+        });
+      }),
+  getTellerBalances: {
+    call: () =>
+      new Promise(resolve => {
+          resolve(new BigNumber(1000000000000000000000));
+      }),
+  },
+  registerPoint: () =>
+    new Promise(resolve => {
+      resolve({
+        logs: [{
+          event: 'RegisterPoint',
+          args: {
+            lat: 1222000, lng: 1239900, rates: 2300, address: '0x000000000000',
+        },
+      }],
+    });
+  }),
+};
+
+export const mock = {
+  setProvider: () => {},
+  at: () => instance,
+  deployed: () => instance,
+};
