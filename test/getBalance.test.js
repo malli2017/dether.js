91588534fece60e6310f6d12c5776c95f1d58f92
ishak <ileghlam@student.42.fr>
2017-10-19 16:37:13 +0200

    added test get teller Balance

--- test/getBalance.test.js
+++ test/getBalance.test.js
@@ -11,23 +11,65 @@ chai.should();
 const { expect } = chai;
 
 describe('getBalance', () => {
+  it('should getBalance is a function', () => {
+    expect(typeof getBalance).to.equal('function');
+  });
 
   it('should works with good arguments', async () => {
     try {
       const test = await getBalance('0x0C6dd5B28707a045f3A0C7429eD3FB9F835Cb623', 'test');
-      console.log('test', test);
+      expect(test).to.equal(1000);
     } catch (e) {
-      console.log('e', e);
+      console.log(e);
+      expect(e).to.equal(null);
     }
   });
 
-  // it('should works with good arguments', async () => {
-  //   try {
-  //     const test = await getBalance('0x0C6dd5B28707a045f3A0C7429eD3FB9F835Cb623', 'test');
-  //     console.log('test', test);
-  //   } catch (e) {
-  //     console.log('e', e);
-  //   }
-  // });
+  it('should works with invalid eth address', async () => {
+    try {
+      await getBalance('0x0C6dd5B28707a045f3A0C7429eD3FB9F835Cb62', 'test');
+    } catch (e) {
+      expect(e).to.not.equal(null);
+    }
+  });
 
+  it('should works with invalid eth address', async () => {
+    try {
+      await getBalance(null, 'test');
+    } catch (e) {
+      expect(e).to.not.equal(null);
+    }
+  });
+
+  it('should works with invalid eth address', async () => {
+    try {
+      await getBalance('1x0C6dd5B28707a045f3A0C7429eD3FB9F835Cb62', 'test');
+    } catch (e) {
+      expect(e).to.not.equal(null);
+    }
+  });
+
+  it('should works with invalid eth address', async () => {
+    try {
+      await getBalance('0C6dd5B28707a045f3A0C7429eD3FB9F835Cb62', 'test');
+    } catch (e) {
+      expect(e).to.not.equal(null);
+    }
+  });
+
+  it('should works with invalid provider URL', async () => {
+    try {
+      await getBalance('0x0C6dd5B28707a045f3A0C7429eD3FB9F835Cb622', null);
+    } catch (e) {
+      expect(e).to.not.equal(null);
+    }
+  });
+
+  it('should works with invalid provider URL', async () => {
+    try {
+      await getBalance('0x0C6dd5B28707a045f3A0C7429eD3FB9F835Cb622', 'https://google.com');
+    } catch (e) {
+      expect(e).to.not.equal(null);
+    }
+  });
 });
