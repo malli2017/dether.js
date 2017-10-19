b9d9c7bee918f0ce861014414bfce0b01e3c6068
ishak <ileghlam@student.42.fr>
2017-10-19 18:49:46 +0200

    new function get contract Storage instance

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,8 +1,8 @@
 import Web3 from 'web3';
 import contract from 'truffle-contract';
 import DetherJson from 'dethercontract/contracts/DetherInterface.json';
-// import DetherStorageJson from 'dethercontract/contracts/DetherStorage.json';
-import { mock } from '../../test/mock/contract-mock';
+import DetherStorageJson from 'dethercontract/contracts/DetherStorage.json';
+import { mock, mockStorage } from '../../test/mock/contract-mock';
 
 export const GAS_PRICE = 25000000000;
 
@@ -10,23 +10,9 @@ export const PROVIDER_URL = process.env.DETHER_ETHEREUM_PROVIDER;
 export const UTILITYWEB3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 /**
- * [DetherContract description]
- * @type {[type]}
- */
-// const DetherContract = contract(DetherJson);
-// DetherContract.setProvider(UTILITYWEB3.currentProvider);
-// const DetherStorageContract = contract(DetherStorageJson);
-// DetherStorageContract.setProvider(UTILITYWEB3.currentProvider);
-
-/**
- * [getContractStorageInstance description]
- * @return {[type]} [description]
- */
-// export const getContractStorageInstance = () => DetherStorageContract.deployed();
-
-/**
  * [getContractInstance description]
- * @return {[type]} [description]
+ * @param  {[type]} provider [description]
+ * @return {[type]}          [description]
  */
 export const getContractInstance = (provider) =>
   new Promise(async (res, rej) => {
@@ -38,4 +24,21 @@ export const getContractInstance = (provider) =>
 
     DetherContract.setProvider(newProvider.currentProvider);
     return res(DetherContract.deployed());
+});
+
+/**
+ * [getContractStorageInstance description]
+ * @param  {[type]} provider [description]
+ * @return {[type]}          [description]
+ */
+export const getContractStorageInstance = (provider) =>
+  new Promise(async (res, rej) => {
+    if (!provider) return rej(new TypeError('Invalid provider URL'));
+    if (provider === 'test') return res(mockStorage.deployed());
+
+    const newProvider = new Web3(new Web3.providers.HttpProvider(provider));
+    const DetherStorageContract = contract(DetherStorageJson);
+
+    DetherStorageContract.setProvider(newProvider.currentProvider);
+    return res(DetherStorageContract.deployed());
 });
