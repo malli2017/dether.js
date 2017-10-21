62b3a16c311f9e1f913e5dde3bea386ad056e869
Ishak Leghlam <ishakleghlam@apples-macbook-pro.home>
2017-10-21 12:58:31 +0200

    update delete function

--- src/tellers/deleteSellPoint.js
+++ src/tellers/deleteSellPoint.js
@@ -1,5 +1,7 @@
 import ethToolbox from 'eth-toolbox';
 
+import { getSignedContractInstance } from '../constants/appConstants';
+
 // gas used = 26497
 // gas price average (mainnet) = 25000000000 wei
 // 50000 * 25000000000 = 0.001250000000000000 ETH
@@ -10,21 +12,31 @@ import ethToolbox from 'eth-toolbox';
  * @param  {string}  password
  * @return {Promise}
  */
-const withdrawAll = (keystore, password) =>
+const withdrawAll = (keystore, password, providerUrl) =>
   new Promise(async (res, rej) => {
-    const key = await ethToolbox.decodeKeystore(keystore, password);
-    if (!key || !key.privateKey || !key.address || !ethToolbox.utils.isAddr(key.address)) {
-      return rej(new TypeError('Invalid keystore or password'));
-    }
-    const dtrContractInstance = await ethToolbox.utils
-      .getSignedWeb3(key.privateKey, key.address);
+    if (!keystore) return rej(new TypeError('Invalid keystore'));
+    if (!password) return rej(new TypeError('Invalid password'));
+    if (!providerUrl) return rej(new TypeError('Invalid provider Url'));
 
-    return res(dtrContractInstance
-      .withdrawAll({
-          from: key.address,
-          gas: 100000,
-          gasPrice: 25000000000,
-        }));
+    try {
+      const key = await ethToolbox.decodeKeystore(keystore, password);
+
+      if (!key || !key.privateKey || !key.address || !ethToolbox.utils.isAddr(key.address)) {
+        return rej(new TypeError('Invalid keystore or password'));
+      }
+
+      const dtrContractInstance =
+        await getSignedContractInstance(key.privateKey, key.address, providerUrl);
+
+      return res(dtrContractInstance
+        .withdrawAll({
+            from: key.address,
+            gas: 100000,
+            gasPrice: 25000000000,
+          }));
+    } catch (e) {
+      return rej(new TypeError(e));
+    }
   });
 
 export default withdrawAll;
