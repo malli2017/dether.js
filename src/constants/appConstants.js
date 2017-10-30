13f3c25f6092ddc3be50ffddc5ce1f71afb42671
Paco <pakokrew@gmail.com>
2017-10-30 13:42:24 +0100

    remove unused libs

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,10 +1,3 @@
-import contract from 'truffle-contract';
-import ethToolbox from 'eth-toolbox';
-import SignerProvider from 'ethjs-provider-signer';
-import { sign } from 'ethjs-signer';
-
-import DetherJson from 'dethercontract/contracts/DetherInterface.json';
-
 export const GAS_PRICE = 25000000000;
 
 const Contracts = {
@@ -14,18 +7,8 @@ const Contracts = {
    * @param  {[type]} address [description]
    * @return {[type]}          [description]
    */
-  async getSignedDetherContract(privateKey, address, providerUrl) {
-    if (!providerUrl) throw new TypeError('Invalid provider URL');
-
-    const DetherContract = contract(DetherJson);
-    const provider = new SignerProvider(providerUrl, {
-      signTransaction: (rawTx, cb) => cb(null, sign(rawTx, ethToolbox.add0x(privateKey))),
-      accounts: cb => cb(null, address),
-    });
-    DetherContract.setProvider(provider);
-    // TODO
-
-    return res(DetherContract.deployed());
+  async getSignedDetherContract() {
+    return {};
   },
 };
 
