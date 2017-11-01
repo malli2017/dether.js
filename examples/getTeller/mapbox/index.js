96c6192fafeea0e4165fa428519a2731c51a46e3
Paco <pakokrew@gmail.com>
2017-11-01 21:39:05 +0100

    update readme

--- examples/getTeller/mapbox/index.js
+++
@@ -1,9 +0,0 @@
-import getCountry from './getCountry';
-import sortCoord from './sortCoord.js';
-
-const mapboxAPI = {
-  getcountrycode: getCountry,
-  sortGpsCoord: sortCoord,
-};
-
-export default mapboxAPI;
